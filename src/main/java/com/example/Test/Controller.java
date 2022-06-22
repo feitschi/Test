cb69169626b962b5d58a05570d10281dbfbd9f90
AndreasFeiertag <andreasfeiertag@outlook.at>
2022-06-22 19:04:00 +0200

    REST Server and time application implemented.

--- src/main/java/com/example/Test/Controller.java
+++ src/main/java/com/example/Test/Controller.java
@@ -2,15 +2,23 @@ package com.example.Test;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
 import java.time.LocalTime;
 
 @RestController
 public class Controller {
 
     Service service = new Service();
-    @GetMapping("/api/time")
 
+    @GetMapping("/api/time")
     public LocalTime localTime(){
         return service.getTime();
     }
+
+    @GetMapping("/api/date")
+    public LocalDate localDate(){
+        return service.getDate();
+    }
+
 }
